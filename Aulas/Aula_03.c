a364f963d87ed69c22e39350636e12a24034fdd4
Vinicius Prado <viniciusprado99@gmail.com>
2025-08-18 20:53:08 -0300

    Update Aula_03.c

--- Aulas/Aula_03.c
+++ Aulas/Aula_03.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int idade = 12;
+    int idade = 12; /*nome de variável, geralmente, é com letra minúscula*/
 
     printf("Sua idade e %d\n", idade);
 
     return 0;
-}
+} 
