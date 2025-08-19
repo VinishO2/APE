f69f613849f8f8fc79d0e693404d380a5865bf86
Vinicius Prado <viniciusprado99@gmail.com>
2025-08-18 21:25:01 -0300

    Update Aula_03.c

--- Aulas/Aula_03.c
+++ Aulas/Aula_03.c
@@ -4,8 +4,10 @@ int main(){
     int idade; /*nome de variável, geralmente, é com letra minúscula*/
 
     printf("Digite a sua idade: ");
-    scanf("%d", &idade); //%d -> quando a informação for inteira// &-> // scanf -> para ler a informação enviada
+    scanf("%d", &idade); //%d -> quando a informação for inteira// &-> aloca o endereço para salvar a informação // scanf -> comando que pede uma entrada via teclado
+    
     printf("Sua idade e %d\n", idade);
+    printf("Minha posicao de memoria e %d\n", &idade);
 
     return 0;
 } 
