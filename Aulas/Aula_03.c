e17caf89bf6e0bd57f500588f9734c53a1ba90c2
Vinicius Prado <viniciusprado99@gmail.com>
2025-08-18 21:14:34 -0300

    Update Aula_03.c

--- Aulas/Aula_03.c
+++ Aulas/Aula_03.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int idade = 12; /*nome de variável, geralmente, é com letra minúscula*/
+    int idade; /*nome de variável, geralmente, é com letra minúscula*/
 
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade); //%d -> quando a informação for inteira// &-> // scanf -> para ler a informação enviada
     printf("Sua idade e %d\n", idade);
 
     return 0;
