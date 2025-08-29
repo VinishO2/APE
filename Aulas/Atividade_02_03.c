8a6bd53e0c8bd05eead085490799e1d80f7ca30d
Vinicius Prado <viniciusprado99@gmail.com>
2025-08-28 21:38:57 -0300

    atividade 02-03

--- Aulas/Atividade_02_03.c
+++ Aulas/Atividade_02_03.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
+    int idade;
+    int cat;
+    char et;
 
-    
+    printf ("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf ("Digite sua categoria (1- Atleta; 2- Amador; 3- Profissional): ");
+    scanf ("%d", &cat);
+
+    if (idade >0 <=120){
+        printf("Idade valida");
+            if(idade <= 12){
+                scanf("%s", &et);
+            } else if (idade >=13 <=17){
+                scanf("%s", &et);
+            } else if (idade >=18 <=59){
+                scanf("%s", &et);
+            } else if (idade >=60){
+                scanf("%s", &et);}
+
+    return 0;
 }
