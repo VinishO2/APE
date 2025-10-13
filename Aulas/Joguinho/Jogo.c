dc3366ad367163e5ad6aa12b29d4c074bd2e6f5a
Vinicius Prado <viniciusprado99@gmail.com>
2025-10-13 20:40:24 -0300

    Trab

---
+++ Aulas/Joguinho/Jogo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main (){
+    char perso [40];
+
+    printf("*********************************************\n");
+    printf("****** Bem-vindo ao (nome do jogo) **********\n");
+    printf("*********************************************\n");
+
+    printf("Voce acordou no seu quarto!\n");
+    printf("Digite o nome do seu personagem: ");
+    scanf("%s", perso);
+
+    printf("CLOVIS - Nossa, seu nome e %s", perso);
+
+
+
+
+
+    return 0;
+}
