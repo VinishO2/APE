6371ba8140416c727b8364b7352aecddc98106a1
Vinicius Prado <viniciusprado99@gmail.com>
2025-10-27 21:33:25 -0300

    Update Aula_13_Pont.c

--- Aulas/Aulas/Aula_13_Pont.c
+++ Aulas/Aulas/Aula_13_Pont.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
 
-void dobrarValorSemRetorno(int n){
-    n = n * 2;
-    printf("Dentro da função: %d\n", n);
+void dobrarValor(int *n){
+    *n = *n * 2;
+    printf("Dentro da função: %d\n", *n);
 }
 
 
@@ -12,7 +12,7 @@ int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
     int x = 5;
-    dobrarValorComRetorno(x);
+    dobrarValor(&x);
     printf("Fora da função: %d\n", x);
 
     return 0;
